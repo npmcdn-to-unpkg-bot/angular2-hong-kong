c44703fb5e1a020b7b9224e4a75978c9e1128864
MxHarr <mitch_harrold@hotmail.com>
2016-07-07 19:21:03 +1000

    Begin

---
+++ app/mock-heroes.ts
@@ -0,0 +1,21 @@
+import { Hero } from './hero';
+
+export var HEROES: Hero[] = [
+  {id: 11, name: 'Hotels'},
+  {id: 12, name: 'Restaurants'},
+  {id: 13, name: 'HK Island'},
+  {id: 14, name: 'Kowloon'},
+  {id: 15, name: 'Outer Islands'},
+  {id: 16, name: 'Macau'},
+  {id: 17, name: 'Phone Applications'},
+  {id: 18, name: 'Useful Info'},
+  {id: 19, name: 'Magma'},
+  {id: 20, name: 'Tornado'}
+];
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
